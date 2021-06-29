186f5d5ca6c2f1ae0c32ae9a473b895f9064fdfb
Aviv-Ratzon <78256805+Aviv-Ratzon@users.noreply.github.com>
2021-06-29 09:11:27 +0300

    Update run_compare.py

--- src/run_compare.py
+++ src/run_compare.py
@@ -1,16 +1,22 @@
 from utils import *
-from config import config
+import config
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
+seq_len = config.TRAIN_CONFIG['seq_len']
+predict_column = config.DATA_CONFIG['predict_column']
+
 def run_compare():
-    single_loss_train = []
-    mututal_loss_train = []
-    single_loss_test = []
-    mututal_loss_test = []
+    single_loss_train_vec = []
+    single_loss_test_vec = []
+    mututal_loss_train_vec = []
+    mututal_loss_test_vec = []
+    target_test_vec = []
+    single_pred_test_vec = []
+    mutual_pred_test_vec = []
     names = []
     names_stocks = []
 
@@ -27,22 +33,25 @@ def run_compare():
             names.append(group_name)
             # Load results of single and mutual models for the current stock
             try:
-                train_data, train_loss_single, train_preds_single, test_data, test_loss_single, test_preds_single = torch.load(path_results + stock_name + '_single_results.pth')
-                train_data_mutual, train_loss_mutual, train_preds_mutual, test_data_mutual, test_loss_mutual, test_preds_mutual = torch.load(path_results + stock_name + '_mutual_results.pth')
+                train_data, single_loss_train, single_pred_train, test_data, single_loss_test, single_pred_test = torch.load(path_results + stock_name + '_single_results.pth')
+                train_data, mututal_loss_train, mututal_pred_train, test_data, mututal_loss_test, mutual_pred_test = torch.load(path_results + stock_name + '_mutual_results.pth')
             except:
                 raise RuntimeError('Missing ' + group_name + ' - ' + stock_name + ' model, reconfig and run train')
-            
+
             # Append losses to lists
-            single_loss_train.append(train_loss_single)
-            single_loss_test.append(test_loss_single)
-            mututal_loss_train.append(train_loss_mutual)
-            mututal_loss_test.append(test_loss_mutual)
+            single_loss_train_vec.append(single_loss_train)
+            single_loss_test_vec.append(single_loss_test)
+            mututal_loss_train_vec.append(mututal_loss_train)
+            mututal_loss_test_vec.append(mututal_loss_test)
+            target_test_vec.append(test_data[seq_len:, predict_column])
+            single_pred_test_vec.append(single_pred_test)
+            mutual_pred_test_vec.append(mutual_pred_test)
 
     names = np.array(names)
     inds = np.where(names[:-1] != names[1:])[0]
 
     # Calculate % change from single model to mutual model for train set and display
-    loss_changes_train = np.array([100*(1-l1/l2) for l1,l2 in zip(single_loss_train,mututal_loss_train)])
+    loss_changes_train = np.array([100*(1-l1/l2) for l1,l2 in zip(single_loss_train_vec,mututal_loss_train_vec)])
     plt.figure()
     plt.bar(np.arange(len(loss_changes_train)), loss_changes_train)
     plt.legend()
@@ -57,7 +66,7 @@ def run_compare():
         plt.text(i, v+0.1*np.sign(v), names_stocks[i], color='k', fontweight='bold', fontsize=8, horizontalalignment='center', alpha=0.7, rotation='vertical')
 
     # Calculate % change from single model to mutual model for test set and display
-    loss_changes_test = np.array([100*(1-l1/l2) for l1,l2 in zip(single_loss_test,mututal_loss_test)])
+    loss_changes_test = np.array([100*(1-l1/l2) for l1,l2 in zip(single_loss_test_vec,mututal_loss_test_vec)])
     plt.figure()
     plt.bar(np.arange(len(loss_changes_test)), loss_changes_test)
     plt.legend()
@@ -84,5 +93,27 @@ def run_compare():
     plt.title('Average Loss Change by Group', fontsize=15)
     plt.xticks(np.arange(len(avg_changes_train)), np.unique(names), rotation=45, fontsize=15)
     plt.gca().yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter())
+
+
+    ind_best = loss_changes_test.argmin()
+    ind_worst = loss_changes_test.argmax()
+
+    fig, (ax1, ax2) = plt.subplots(2,1)
+    ax1.plot(target_test_vec[ind_best], label='target')
+    ax1.plot(single_pred_test_vec[ind_best], label='single model pred')
+    ax1.plot(mutual_pred_test_vec[ind_best], label='mutual model pred')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('pct change')
+    ax1.set_title('Most Improved Model')
+    ax1.legend()
+
+    ax2.plot(target_test_vec[ind_worst], label='target')
+    ax2.plot(single_pred_test_vec[ind_worst], label='single model pred')
+    ax2.plot(mutual_pred_test_vec[ind_worst], label='mutual model pred')
+    ax2.set_xlabel('Date')
+    ax2.set_ylabel('pct change')
+    ax2.set_title('Least Improved Model')
+    ax2.legend()
+
 
     plt.show()
