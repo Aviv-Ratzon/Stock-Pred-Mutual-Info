c57aa008c38f3de967dbe545ea2f641102b61603
Aviv-Ratzon <78256805+Aviv-Ratzon@users.noreply.github.com>
2021-06-30 16:35:15 +0300

    Update run_train.py

--- src/run_train.py
+++ src/run_train.py
@@ -1,6 +1,6 @@
 from train import *
 from utils import *
-from config import config
+import config
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -31,38 +31,44 @@ def run_train():
             train_data, test_data = stock_data
 
             # Train single model
-            print('Training ' + stock_name + ' single model.............')
-            model_single, test_losses_single, train_losses_single = train(train_data, test_data)
-            print('==> Saving model ...')
-            state = {'net': model_single.state_dict()}
-            torch.save(state, path_ckpt + stock_name + '_single_ckpt.pth')
+            filename = path_ckpt + stock_name + '_single_ckpt.pth'
+            if not os.path.isfile(filename) or config.RUN_CONFIG['retrain']:
+                print('Training ' + stock_name + ' single model.............')
+                model_single, test_losses_single, train_losses_single = train(train_data, test_data)
+                print('==> Saving model ...')
+                state = {'net': model_single.state_dict()}
+                torch.save(state, filename)
 
-            # Evaluate single model
-            train_loss_single, train_preds_single = evaluate(model_single, train_data, nn.MSELoss())
-            test_loss_single, test_preds_single = evaluate(model_single, test_data, nn.MSELoss())
+                # Evaluate single model
+                train_loss_single, train_preds_single = evaluate(model_single, train_data, nn.MSELoss())
+                test_loss_single, test_preds_single = evaluate(model_single, test_data, nn.MSELoss())
+                # Save data of losses and prediction for both train and test
+                print('Test loss for ' + stock_name + ' single model = {:.5f}'.format(test_loss_single))
+                print('Saving results for ' + stock_name + ' single.............')
+                torch.save([train_data, train_loss_single, train_preds_single,
+                            test_data, test_loss_single, test_preds_single], path_results + stock_name + '_single_results.pth')
 
-
-            # Concatenate other stocks closing prices to current stock
-            train_data_mutual = torch.cat([train_data] + [v[0][:,[config.DATA_CONFIG['predict_column']]] for k, v in stock_datas.items() if k != stock_name], axis=1)
-            test_data_mutual = torch.cat([test_data] + [v[1][:,[config.DATA_CONFIG['predict_column']]] for k, v in stock_datas.items() if k != stock_name], axis=1)
 
             # Train mutual model
-            print('Training ' + stock_name + ' mutual model.............')
-            model_mutual, test_losses_mutual, train_losses_mutual = train(train_data_mutual, test_data_mutual)
-            print('==> Saving model ...')
-            state = {'net': model_mutual.state_dict()}
-            torch.save(state, path_ckpt + stock_name + '_mutual_ckpt.pth')
+            filename = path_ckpt + stock_name + '_mutual_ckpt.pth'
+            if not os.path.isfile(filename) or config.RUN_CONFIG['retrain']:
+                # Concatenate other stocks closing prices to current stock
+                train_data_mutual = torch.cat([train_data] + [v[0][:,[config.DATA_CONFIG['predict_column']]] for k, v in stock_datas.items() if k != stock_name], axis=1)
+                test_data_mutual = torch.cat([test_data] + [v[1][:,[config.DATA_CONFIG['predict_column']]] for k, v in stock_datas.items() if k != stock_name], axis=1)
 
-            # Evaluate mutual model
-            train_loss_mutual, train_preds_mutual = evaluate(model_mutual, train_data_mutual, nn.MSELoss())
-            test_loss_mutual, test_preds_mutual = evaluate(model_mutual, test_data_mutual, nn.MSELoss())
+                print('Training ' + stock_name + ' mutual model.............')
+                model_mutual, test_losses_mutual, train_losses_mutual = train(train_data_mutual, test_data_mutual)
+                print('==> Saving model ...')
+                state = {'net': model_mutual.state_dict()}
+                torch.save(state, filename)
 
-            print('Test loss for ' + stock_name + ' single model = {:.5f}'.format(test_loss_single))
-            print('Test loss for ' + stock_name + ' mutual model = {:.5f}'.format(test_loss_mutual))
+                # Evaluate mutual model
+                train_loss_mutual, train_preds_mutual = evaluate(model_mutual, train_data_mutual, nn.MSELoss())
+                test_loss_mutual, test_preds_mutual = evaluate(model_mutual, test_data_mutual, nn.MSELoss())
 
-            # Save data of losses and prediction for both train and test of single and mutual models
-            print('Saving results for ' + stock_name + '.............')
-            torch.save([train_data, train_loss_single, train_preds_single,
-                        test_data, test_loss_single, test_preds_single], path_results + stock_name + '_single_results.pth')
-            torch.save([train_data_mutual, train_loss_mutual, train_preds_mutual,
-                        test_data_mutual, test_loss_mutual, test_preds_mutual], path_results + stock_name + '_mutual_results.pth')
+                # Save data of losses and prediction for both train and test
+                print('Test loss for ' + stock_name + ' mutual model = {:.5f}'.format(test_loss_mutual))
+                # Save data of losses and prediction for both train and test of single and mutual models
+                print('Saving results for ' + stock_name + ' mutual.............')
+                torch.save([train_data_mutual, train_loss_mutual, train_preds_mutual,
+                            test_data_mutual, test_loss_mutual, test_preds_mutual], path_results + stock_name + '_mutual_results.pth')
